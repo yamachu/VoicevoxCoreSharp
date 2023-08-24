f5614774437b4bb86320d4c9703ff0866155b146
Yusuke Yamada <yamachu.dev@gmail.com>
2023-08-25 01:46:18 +0900

    CIでテストを回せるように (#1)
    * リネームやファイルの配置をいい感じに
    
    * resourcesディレクトリ内にOpenJTalkの辞書を格納するようにする
    
    * CIおためし
    
    * native libraryが読まれないのでdebug
    
    * Linuxは命名規則が違った
    
    * remove detailed output
    
    * dotnetのテストではsubmodulesは不要
    
    * Revert "dotnetのテストではsubmodulesは不要"
    
    This reverts commit fdea68a25b55e5d78045c74f0b0273bf802e3249.
    ディレクトリが生成されないからrestore出来なかった…

--- tests/VoicevoxCoreSharp.Core.Tests/Consts.cs
+++ tests/VoicevoxCoreSharp.Core.Tests/Consts.cs
@@ -5,7 +5,7 @@ namespace VoicevoxCoreSharp.Core.Tests
 {
     public class Consts
     {
-        public static readonly string OpenJTalkDictDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", "open_jtalk_dic_utf_8-1.11");
+        public static readonly string OpenJTalkDictDir = Path.Combine(Helper.GetProjectDirectory(), "resources", "open_jtalk_dic_utf_8-1.11");
         public static readonly string SampleVoiceModel = Path.Combine(Helper.GetBaseVoicevoxCoreDirectory(), "model", "sample.vvm");
     }
 }
