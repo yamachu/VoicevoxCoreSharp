3fb16fb1ad8b097b4a171f40cbf5f44941d405cd
yamachu <yamachu.dev@gmail.com>
2023-08-26 01:27:56 +0900

    SampleVoiceはresourcesディレクトリのものを見る

--- tests/VoicevoxCoreSharp.Core.Tests/Consts.cs
+++ tests/VoicevoxCoreSharp.Core.Tests/Consts.cs
@@ -6,6 +6,6 @@ namespace VoicevoxCoreSharp.Core.Tests
     public class Consts
     {
         public static readonly string OpenJTalkDictDir = Path.Combine(Helper.GetProjectDirectory(), "resources", "open_jtalk_dic_utf_8-1.11");
-        public static readonly string SampleVoiceModel = Path.Combine(Helper.GetBaseVoicevoxCoreDirectory(), "model", "sample.vvm");
+        public static readonly string SampleVoiceModel = Path.Combine(Helper.GetProjectDirectory(), "resources", "model", "sample.vvm");
     }
 }
