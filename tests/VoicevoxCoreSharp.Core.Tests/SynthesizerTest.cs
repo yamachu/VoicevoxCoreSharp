e2b3673f9cc530b1b9afee4a880568ccaef13621
yamachu <yamachu.dev@gmail.com>
2023-08-26 01:40:11 +0900

    LoadModelの時もcrashする場合が出てきたので、Assertionを追加する

--- tests/VoicevoxCoreSharp.Core.Tests/SynthesizerTest.cs
+++ tests/VoicevoxCoreSharp.Core.Tests/SynthesizerTest.cs
@@ -15,9 +15,10 @@ namespace VoicevoxCoreSharp.Core.Tests
             var synthesizerResult = Synthesizer.New(openJtalk, initializeOptions, out var synthesizer);
 
             VoiceModel.New(Consts.SampleVoiceModel, out var voiceModel);
-            synthesizer.LoadVoiceModel(voiceModel);
+            var loadResult = synthesizer.LoadVoiceModel(voiceModel);
 
             Assert.Equal(ResultCode.RESULT_OK, synthesizerResult);
+            Assert.Equal(ResultCode.RESULT_OK, loadResult);
             Assert.NotEmpty(synthesizer.MetasJson);
 
             var ttsResult = synthesizer.Tts("こんにちは", 0, TtsOptions.Default(), out var wavLength, out var wav);
