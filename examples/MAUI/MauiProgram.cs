68e0b4e42e76c0c2e2656af67daf9db2d4498cc7
yamachu <yamachu.dev@gmail.com>
2023-08-31 01:12:40 +0900

    いったん動くところまで

--- examples/MAUI/MauiProgram.cs
+++ examples/MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace MAUI;
 
@@ -9,6 +10,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
