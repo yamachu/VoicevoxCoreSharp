df749de63c5c3815f9dabca62fc32a1bcacade47
yamachu <yamachu.dev@gmail.com>
2024-09-27 02:38:34 +0900

    OnnxruntimeのLoadテストをCIでも実行できるように、各種プラットフォームのサポートを追加

--- tests/VoicevoxCoreSharp.Core.Tests/OnnxruntimeTest.cs
+++ tests/VoicevoxCoreSharp.Core.Tests/OnnxruntimeTest.cs
@@ -11,12 +11,15 @@ namespace VoicevoxCoreSharp.Core.Tests
         [Fact]
         public void LoadOnce()
         {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            // See: VoicevoxCoreSharp.Core.Tests.csproj
+            var libraryPath = 0 switch
             {
-                Assert.True(true, "TODO: GitHub Actionsでも試せるようにする");
-                return;
-            }
-            var option = new LoadOnnxruntimeOptions("libonnxruntime_another_path.dylib");
+                var _ when RuntimeInformation.IsOSPlatform(OSPlatform.Windows) => "onnxruntime_another_path.dll",
+                var _ when RuntimeInformation.IsOSPlatform(OSPlatform.Linux) => "libonnxruntime_another_path.so",
+                var _ when RuntimeInformation.IsOSPlatform(OSPlatform.OSX) => "libonnxruntime_another_path.dylib",
+                _ => throw new PlatformNotSupportedException()
+            };
+            var option = new LoadOnnxruntimeOptions(libraryPath);
             var result = Onnxruntime.LoadOnce(option, out var onnruntime);
 
             Assert.Equal(ResultCode.RESULT_OK, result);
