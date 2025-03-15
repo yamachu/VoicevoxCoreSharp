c2f0bb3408088445e1c14e658aa39aa3d4f534f3
yamachu <yamachu.dev@gmail.com>
2025-03-15 10:04:27 +0900

    モバイル端末ではInitializeを呼ばせて、resolverを設定させる

--- examples/MAUI/Platforms/Android/MainApplication.cs
+++ examples/MAUI/Platforms/Android/MainApplication.cs
@@ -1,15 +1,18 @@
 ﻿using Android.App;
 using Android.Runtime;
+using VoicevoxCoreSharp.MAUI;
 
 namespace MAUI;
 
 [Application]
 public class MainApplication : MauiApplication
 {
-	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-		: base(handle, ownership)
-	{
-	}
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+        // VoicevoxCoreのネイティブ依存のパス解決のため、ライブラリが読み込まれる前に実行する
+        CoreHelper.Initialize(null);
+    }
 
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
