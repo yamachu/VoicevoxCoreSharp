abe4963868a486f4c44e4ffd9d5813190f07b831
yamachu <yamachu.dev@gmail.com>
2025-03-30 17:52:14 +0900

    最新のcoreのinterfaceに合わせる

--- examples/UnitySample/Assets/SampleVoicevoxCoreSharpScript.cs
+++ examples/UnitySample/Assets/SampleVoicevoxCoreSharpScript.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using VoicevoxCoreSharp.Core;
-using VoicevoxCoreSharp.Core.Struct;
 using VoicevoxCoreSharp.Core.Enum;
+using VoicevoxCoreSharp.Core.Struct;
 
 public class SampleVoicevoxCoreSharpScript : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class SampleVoicevoxCoreSharpScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var openJTalkDictPath = System.IO.Path.Combine(Application.streamingAssetsPath, "voicevox_core/open_jtalk_dic_utf_8-1.11/");
+        var openJTalkDictPath = System.IO.Path.Combine(Application.streamingAssetsPath, "voicevox_core/dict/open_jtalk_dic_utf_8-1.11");
 
         Debug.Log("coreの初期化中");
 
@@ -27,7 +28,7 @@ public class SampleVoicevoxCoreSharpScript : MonoBehaviour
         }
 
         // TODO: This is platform dependent code, FIXME
-        var loadOnnxruntimeOptions = LoadOnnxruntimeOptions.Default();
+        var loadOnnxruntimeOptions = new LoadOnnxruntimeOptions(Path.Combine(Application.dataPath, "Plugins", "runtimes", "osx.10.14-arm64", "native", "libvoicevox_onnxruntime.1.17.3.dylib"));
         if (Onnxruntime.LoadOnce(loadOnnxruntimeOptions, out var onnxruntime) != ResultCode.RESULT_OK)
         {
             Debug.LogError("Failed to initialize onnxruntime");
@@ -42,7 +43,7 @@ public class SampleVoicevoxCoreSharpScript : MonoBehaviour
 
         using (openJtalk) { }
 
-        result = VoiceModelFile.New(System.IO.Path.Combine(Application.streamingAssetsPath, "voicevox_core/model/sample.vvm"), out var voiceModel);
+        result = VoiceModelFile.New(System.IO.Path.Combine(Application.streamingAssetsPath, "voicevox_core/models/vvms/0.vvm"), out var voiceModel);
         if (result != ResultCode.RESULT_OK)
         {
             Debug.LogError(result.ToMessage());
