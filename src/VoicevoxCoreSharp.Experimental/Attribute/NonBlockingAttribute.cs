eb593a2c19b942597e33de4c47c7955b4e73e107
yamachu <yamachu.dev@gmail.com>
2025-04-02 02:32:34 +0900

    SourceGeneratorからどのメソッドを対象にすればいいかわかるように、適当なAttributeを生やす

---
+++ src/VoicevoxCoreSharp.Experimental/Attribute/NonBlockingAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace VoicevoxCoreSharp.Experimental.Attribute
+{
+    [System.AttributeUsage(System.AttributeTargets.Method)]
+    internal sealed class NonBlockingAttribute : System.Attribute
+    {
+        public NonBlockingAttribute() { }
+    }
+
+}
