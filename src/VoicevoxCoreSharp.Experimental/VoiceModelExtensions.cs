525d2be6eb589d54197ded51f3ef1f896a445d31
Yusuke Yamada <yamachu.dev@gmail.com>
2026-02-21 15:07:35 +0900

    足りてないOpenAsyncがあったので足す (#265)

--- src/VoicevoxCoreSharp.Experimental/VoiceModelExtensions.cs
+++ src/VoicevoxCoreSharp.Experimental/VoiceModelExtensions.cs
@@ -8,7 +8,11 @@ namespace VoicevoxCoreSharp.Experimental
 {
     public static partial class VoiceModelFileExtensions
     {
+        [Obsolete("Use VoiceModelFile.OpenAsync(string modelPath) instead.")]
         [NonBlocking]
         public static partial Task<VoiceModelFile> NewAsync(string modelPath);
+
+        [NonBlocking]
+        public static partial Task<VoiceModelFile> OpenAsync(string modelPath);
     }
 }
