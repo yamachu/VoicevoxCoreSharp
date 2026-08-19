8bf0f32628af90e92590e762114e8ad4ddb090c8
yamachu <yamachu.dev@gmail.com>
2026-08-20 01:21:40 +0900

    StyleIdとVoiceModelIdにNewTypePatternを導入

---
+++ src/VoicevoxCoreSharp.Core.Unity/Runtime/Script/Type/StyleId.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+
+namespace VoicevoxCoreSharp.Core
+{
+    public readonly struct StyleId : IEquatable<StyleId>
+    {
+        public uint Value { get; }
+
+        public StyleId(uint value)
+        {
+            Value = value;
+        }
+
+        public bool Equals(StyleId other)
+        {
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is StyleId other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public static explicit operator uint(StyleId styleId)
+        {
+            return styleId.Value;
+        }
+
+        public static explicit operator StyleId(uint value)
+        {
+            return new StyleId(value);
+        }
+
+        public static bool operator ==(StyleId left, StyleId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(StyleId left, StyleId right)
+        {
+            return !left.Equals(right);
+        }
+    }
+
+    internal static class StyleIdExt
+    {
+        internal static uint ToNative(this StyleId styleId)
+        {
+            return styleId.Value;
+        }
+    }
+}
---
+++ src/VoicevoxCoreSharp.Core/Type/VoiceModelId.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace VoicevoxCoreSharp.Core
+{
+    public readonly struct VoiceModelId : IEquatable<VoiceModelId>
+    {
+        public static VoiceModelId Empty { get; } = new VoiceModelId(Guid.Empty);
+
+        public Guid Value { get; }
+
+        public VoiceModelId(Guid value)
+        {
+            Value = value;
+        }
+
+        public VoiceModelId(string value) : this(Guid.Parse(value))
+        {
+        }
+
+        public bool Equals(VoiceModelId other)
+        {
+            return Value.Equals(other.Value);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is VoiceModelId other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString("D");
+        }
+
+        public static explicit operator Guid(VoiceModelId voiceModelId)
+        {
+            return voiceModelId.Value;
+        }
+
+        public static explicit operator VoiceModelId(Guid value)
+        {
+            return new VoiceModelId(value);
+        }
+
+        public static explicit operator VoiceModelId(string value)
+        {
+            return new VoiceModelId(value);
+        }
+
+        public static explicit operator string(VoiceModelId voiceModelId)
+        {
+            return voiceModelId.ToString();
+        }
+
+        public static bool operator ==(VoiceModelId left, VoiceModelId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(VoiceModelId left, VoiceModelId right)
+        {
+            return !left.Equals(right);
+        }
+    }
+
+    internal static class VoiceModelIdExt
+    {
+        internal static unsafe VoiceModelId FromNative(byte* ptr)
+        {
+            return new VoiceModelId(NativeUuid.ToGuid(ptr));
+        }
+
+        internal static byte[] ToNative(this VoiceModelId voiceModelId)
+        {
+            return NativeUuid.ToUUIDv4ByteArray(voiceModelId.Value);
+        }
+    }
+}
